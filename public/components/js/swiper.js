54dff1c1c21c523236c8da87cf01a90102827c39
zefirkaxl <print.yarish@gmail.com>
2024-07-20 22:02:06 +0300

    first commit

---
+++ public/components/js/swiper.js
@@ -0,0 +1,40 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+let swiperInstance;
+
+function initializeSwiper() {
+  const height = window.innerHeight;
+  const width = window.innerWidth;
+
+
+  // Перевірка на висоту і ширину
+  if (height <= 850 || width <= 1242) {
+    if (!swiperInstance) {
+      swiperInstance = new Swiper(".swiper", {
+        modules: [Navigation],
+        navigation: {
+          nextEl: ".swiper-button-right",
+          prevEl: ".swiper-button-left",
+        },
+        slidesPerView: 'auto', 
+        spaceBetween: 10,
+      });
+    }
+  } else {
+    if (swiperInstance) {
+      swiperInstance.destroy();
+      swiperInstance = null;
+    }
+  }
+}
+
+// Викликати при завантаженні сторінки та при зміні розміру вікна
+document.addEventListener("DOMContentLoaded", initializeSwiper);
+window.addEventListener("resize", initializeSwiper);
+
+// Викликати при завантаженні сторінки та при зміні розміру вікна
+window.addEventListener("load", initializeSwiper);
